cb695b1d4a5b350c9e31db415410884632322d06
vidit <136428327+Vidit93@users.noreply.github.com>
2023-08-26 14:00:48 +0530

    update App.js

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import {View,Text} from 'react-native'
-import HomeScreen from './src/screens/HomeScreen'
+import {Text,View} from 'react-native'
+import Startscreen from './src/screens/Startscreen'
+import Registerscreen from './src/screens/Registerscreen'
+import Loginscreen from './src/screens/Loginscreen'
+import Productinfoscreen from './src/screens/Productinfocardscreen'
 
 export default function App(){
-  return(
-    <>
-    <HomeScreen/>
-    </>
-  )
-}
+    return(
+        <Productinfoscreen/>
+    )
+} 
